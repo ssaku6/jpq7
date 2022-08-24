4723148f2fc137a76491dd05ecbbec7be0651b3a
Toshihide Imaruoka <imaruoka@gmail.com>
2022-08-24 11:12:25 +0900

    mod instruction 2

--- main.js
+++ main.js
@@ -20,20 +20,18 @@ var repo_site = "https://imaru.github.io/jqTemplate/";
  /* define instructions trial */
  var instructions = {
    type: "html-keyboard-response",
-   stimulus: `
-     <p>In this experiment, a circle will appear in the center 
-     of the screen.</p><p>If the circle is <strong>blue</strong>, 
-     press the letter F on the keyboard as fast as you can.</p>
-     <p>If the circle is <strong>orange</strong>, press the letter J 
-     as fast as you can.</p>
-     <div style='width: 700px;'>
-     <div style='float: left;'><img src='img/blue.png'></img>
-     <p class='small'><strong>Press the F key</strong></p></div>
-     <div style='float: right;'><img src='img/orange.png'></img>
-     <p class='small'><strong>Press the J key</strong></p></div>
-     </div>
-     <p>Press any key to begin.</p>
-   `,
+   stimulus: "<p>In this experiment, a circle will appear in the center " +
+     " of the screen.</p><p>If the circle is <strong>blue</strong>, " +
+     " press the letter F on the keyboard as fast as you can.</p> " +
+     " <p>If the circle is <strong>orange</strong>, press the letter J " +
+     " as fast as you can.</p> " +
+     " <div style='width: 700px;'>" +
+     " <div style='float: left;'><img src=" + repo_site + "'img/blue.png'></img>" +
+     " <p class='small'><strong>Press the F key</strong></p></div>" + 
+     " <div style='float: right;'><img src=" + repo_site + "'img/orange.png'></img> " +
+     " <p class='small'><strong>Press the J key</strong></p></div>" +
+     "</div>" +
+     " <p>Press any key to begin.</p> ",
    post_trial_gap: 2000
  };
  timeline.push(instructions);
