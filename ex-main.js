795e6c5dde44b761310570d3933f78b060ad7c0f
sakurai Yuki <c6301309@st.kanazawa-it.ac.jp>
2024-09-24 18:54:08 +0900

    a

--- ex-main.js
+++ ex-main.js
@@ -40,8 +40,12 @@ var image_trial = {
 var welcome2 = {
     type: "html-keyboard-response",
     stimulus: `
-    絵画を評価していた時間を再現してください。<br>
-    Spaceキーを押して操作してください。`,
+        絵画を評価していた時間を再現してください。<br>
+        次の画面では、真っ白な画面が表示れます。<br>
+        その画面ではSpaceキーを長押しすると四角形が表示され、押し続けることを止めると消えます。<br>
+        絵画を見ていた時間と同じ時間、四角形を表示させてください。<br>
+        <strong>spaceキーを押す操作は1度しかできないので注意してください</strong><br>
+        enterキーで進む`,
     choices: ["Enter"]
 };
 
