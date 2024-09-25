d3b34b88e63bb3cf9b757dbe97a43d161469c34f
sakurai Yuki <c6301309@st.kanazawa-it.ac.jp>
2024-09-25 16:42:40 +0900

    a

--- ex-main.js
+++ ex-main.js
@@ -118,8 +118,10 @@ var repo_site = "https://ssaku6.github.io/jpq7/";
         }
     };
 
-   
-    jsPsych.init({
-        timeline: [preload, welcome, hello_trial, welcome2],
-    });
+   var produce={
+    timeline:[preload, welcome, hello_trial, welcome2, space_key_trial]
+   }
+    
+    
+    timeline.push(produce);
 
