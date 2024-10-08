1b8ab19cc4eda86cff866a15efd7789385011a8d
sakurai Yuki <c6301309@st.kanazawa-it.ac.jp>
2024-10-08 16:57:53 +0900

    a

--- ex-main.js
+++ ex-main.js
@@ -1,100 +1,15 @@
 var repo_site = "https://ssaku6.github.io/jpq7/";
 
 
-
-
-/* create timeline */
-var timeline = [];
-
-/* preload images */
-var preload = {
-  type: 'preload',
-  images: [repo_site+'img/blue.png', repo_site+'img/orange.png']
-}
-timeline.push(preload);
-
-/* define welcome message trial */
-var welcome = {
-  type: "html-keyboard-response",
-  stimulus: "Welcome to the experiment. Press any key to begin."
+var welcome2 = {
+    type: "html-keyboard-response",
+    stimulus: `
+    絵画を評価していた時間を再現してください。<br>
+    次の画面では、真っ白な画面が表示れます。<br>
+    その画面ではSpaceキーを長押しすると四角形が表示され、押し続けることを止めると消えます。<br>
+    絵画を見ていた時間と同じ時間、四角形を表示させてください。<br>
+    <strong>spaceキーを押す操作は1度しかできないので注意してください</strong>`,
+    choices: ["Enter"]
 };
-timeline.push(welcome);
-
-/* define instructions trial */
-var instructions = {
-  type: "html-keyboard-response",
-  stimulus: "<p>In this experiment, a circle will appear in the center " +
-    " of the screen.</p><p>If the circle is <strong>blue</strong>, " +
-    " press the letter F on the keyboard as fast as you can.</p> " +
-    " <p>If the circle is <strong>orange</strong>, press the letter J " +
-    " as fast as you can.</p> " +
-    " <div style='width: 700px;'>" +
-    " <div style='float: left;'><img src=" + repo_site + "'img/blue.png'></img>" +
-    " <p class='small'><strong>Press the F key</strong></p></div>" + 
-    " <div style='float: right;'><img src=" + repo_site + "'img/orange.png'></img> " +
-    " <p class='small'><strong>Press the J key</strong></p></div>" +
-    "</div>" +
-    " <p>Press any key to begin.</p> ",
-  post_trial_gap: 2000
-};
-timeline.push(instructions);
-
-/* test trials */
-var test_stimuli = [
-  { stimulus: repo_site+"img/blue.png",  correct_response: 'f'},
-  { stimulus: repo_site+"img/orange.png",  correct_response: 'j'}
-];
-
-var fixation = {
-  type: 'html-keyboard-response',
-  stimulus: '<div style="font-size:60px;">+</div>',
-  choices: jsPsych.NO_KEYS,
-  trial_duration: function(){
-    return jsPsych.randomization.sampleWithoutReplacement([250, 500, 750, 1000, 1250, 1500, 1750, 2000], 1)[0];
-  },
-  data: {
-    task: 'fixation'
-  }
-}
-
-var test = {
-  type: "image-keyboard-response",
-  stimulus: jsPsych.timelineVariable('stimulus'),
-  choices: ['f', 'j'],
-  data: {
-    task: 'response',
-    correct_response: jsPsych.timelineVariable('correct_response')
-  },
-  on_finish: function(data){
-    data.correct = jsPsych.pluginAPI.compareKeys(data.response, data.correct_response);
-  }
-}
-
-var test_procedure = {
-  timeline: [fixation, test],
-  timeline_variables: test_stimuli,
-  repetitions: 5,
-  randomize_order: true
-}
-timeline.push(test_procedure);
-
-/* define debrief */
-
-var debrief_block = {
-  type: "html-keyboard-response",
-  stimulus: function() {
-
-    var trials = jsPsych.data.get().filter({task: 'response'});
-    var correct_trials = trials.filter({correct: true});
-    var accuracy = Math.round(correct_trials.count() / trials.count() * 100);
-    var rt = Math.round(correct_trials.select('rt').mean());
-
-    return `<p>You responded correctly on ${accuracy}% of the trials.</p>
-      <p>Your average response time was ${rt}ms.</p>
-      <p>Press any key to complete the experiment. Thank you!</p>`;
-
-  }
-};
-timeline.push(debrief_block);
 
 
