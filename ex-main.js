8c51d960adff98dba1a4ade8809f4ebc210f021e
sakurai Yuki <c6301309@st.kanazawa-it.ac.jp>
2024-10-08 17:05:18 +0900

    a

--- ex-main.js
+++ ex-main.js
@@ -1,17 +1,13 @@
 var repo_site = "https://ssaku6.github.io/jpq7/";
 
 
-var welcome2 = {
-    type: "html-keyboard-response",
-    stimulus: `
-    絵画を評価していた時間を再現してください。<br>
-    次の画面では、真っ白な画面が表示れます。<br>
-    その画面ではSpaceキーを長押しすると四角形が表示され、押し続けることを止めると消えます。<br>
-    絵画を見ていた時間と同じ時間、四角形を表示させてください。<br>
-    <strong>spaceキーを押す操作は1度しかできないので注意してください</strong>`,
-    choices: ["Enter"]
-};
+ /* preload images */
+ var preload = {
+    type: 'preload',
+    images: [repo_site+'img/blue.png', repo_site+'img/orange.png']
+  };
+  
 
 jsPsych.init({
-    timeline: [ welcome2,],
+    timeline: [ preload,],
 });
