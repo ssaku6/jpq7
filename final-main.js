f0b388889618809f6214b7465c840cb9a6995dac
sakurai Yuki <c6301309@st.kanazawa-it.ac.jp>
2024-12-11 20:49:05 +0900

    a

--- final-main.js
+++ final-main.js
@@ -87,19 +87,19 @@ for (var i = 0; i < 1; i++) {
     trials.push({
         image: concreteGroups[1][i],    // 2番目の具象画グループから画像
         set: conditionSets[0],          // 最初の形容詞対セット
-        art: concreteGroups[0][i]      // 'art' として画像を記録
+        art: concreteGroups[1][i]      // 'art' として画像を記録
     });
 
     // 抽象画のグループから画像を取り出して条件セットを組み合わせる
     trials.push({
         image: abstractGroups[0][i],    // 1番目の抽象画グループから画像
         set: conditionSets[1],          // 次の形容詞対セット
-        art: concreteGroups[0][i]      // 'art' として画像を記録
+        art: abstractGroups[0][i]      // 'art' として画像を記録
     });
     trials.push({
         image: abstractGroups[1][i],    // 2番目の抽象画グループから画像
         set: conditionSets[1],          // 次の形容詞対セット
-        art: concreteGroups[0][i]      // 'art' として画像を記録
+        art: abstractGroups[1][i]      // 'art' として画像を記録
     });
 }
 
