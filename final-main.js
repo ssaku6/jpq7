8700b4f5d66f6a242b6d4d85d94871978f72b523
sakurai Yuki <c6301309@st.kanazawa-it.ac.jp>
2024-12-15 16:08:27 +0900

    a

--- final-main.js
+++ final-main.js
@@ -5,64 +5,65 @@ var repo_site = "https://ssaku6.github.io/jpq7/";
 var timeline = [];
 
 var test_stimuli_set1 = [
-    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: 'img2/01.jpg'},
-    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: 'img2/02.jpg'},
-    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: 'img2/03.jpg'},
-    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: 'img2/04.jpg'},
-    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: 'img2/05.jpg'},
-    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: 'img2/06.jpg'},
-    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: 'img2/07.jpg'},
-    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: 'img2/08.jpg'},
-    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: 'img2/09.jpg'},
-    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: 'img2/10.jpg'},
-    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: 'img2/11.jpg'},
-    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: 'img2/12.jpg'}
-  ];
-  
-  var test_stimuli_set2 = [
-    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: 'img2/13.jpg'},
-    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: 'img2/14.jpg'},
-    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: 'img2/15.jpg'},
-    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: 'img2/16.jpg'},
-    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: 'img2/17.jpg'},
-    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: 'img2/18.jpg'},
-    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: 'img2/19.jpg'},
-    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: 'img2/20.jpg'},
-    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: 'img2/21.jpg'},
-    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: 'img2/22.jpg'},
-    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: 'img2/23.jpg'},
-    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: 'img2/24.jpg'},
-  ];
-  
-  var test_stimuli_set3 = [
-    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: 'img2/25.jpg'},
-    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: 'img2/26.jpg'},
-    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: 'img2/27.jpg'},
-    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: 'img2/28.jpg'},
-    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: 'img2/29.jpg'},
-    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: 'img2/30.jpg'},
-    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: 'img2/31.jpg'},
-    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: 'img2/32.jpg'},
-    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: 'img2/33.jpg'},
-    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: 'img2/34.jpg'},
-    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: 'img2/35.jpg'},
-    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: 'img2/36.jpg'}
-  ];
-  
-  var test_stimuli_set4 = [
-    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: 'img2/37.jpg'},
-    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: 'img2/38.jpg'},
-    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: 'img2/39.jpg'},
-    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: 'img2/40.jpg'},
-    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: 'img2/41.jpg'},
-    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: 'img2/42.jpg'},
-    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: 'img2/43.jpg'},
-    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: 'img2/44.jpg'},
-    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: 'img2/45.jpg'},
-    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: 'img2/46.jpg'},
-    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: 'img2/47.jpg'},
-    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: 'img2/48.jpg'},
-  ];
+    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: repo_site + 'img2/01.jpg'},
+    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: repo_site + 'img2/02.jpg'},
+    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: repo_site + 'img2/03.jpg'},
+    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: repo_site + 'img2/04.jpg'},
+    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: repo_site + 'img2/05.jpg'},
+    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: repo_site + 'img2/06.jpg'},
+    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: repo_site + 'img2/07.jpg'},
+    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: repo_site + 'img2/08.jpg'},
+    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: repo_site + 'img2/09.jpg'},
+    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: repo_site + 'img2/10.jpg'},
+    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: repo_site + 'img2/11.jpg'},
+    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: repo_site + 'img2/12.jpg'}
+];
+
+var test_stimuli_set2 = [
+    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: repo_site + 'img2/13.jpg'},
+    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: repo_site + 'img2/14.jpg'},
+    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: repo_site + 'img2/15.jpg'},
+    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: repo_site + 'img2/16.jpg'},
+    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: repo_site + 'img2/17.jpg'},
+    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: repo_site + 'img2/18.jpg'},
+    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: repo_site + 'img2/19.jpg'},
+    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: repo_site + 'img2/20.jpg'},
+    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: repo_site + 'img2/21.jpg'},
+    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: repo_site + 'img2/22.jpg'},
+    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: repo_site + 'img2/23.jpg'},
+    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: repo_site + 'img2/24.jpg'},
+];
+
+var test_stimuli_set3 = [
+    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: repo_site + 'img2/25.jpg'},
+    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: repo_site + 'img2/26.jpg'},
+    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: repo_site + 'img2/27.jpg'},
+    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: repo_site + 'img2/28.jpg'},
+    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: repo_site + 'img2/29.jpg'},
+    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: repo_site + 'img2/30.jpg'},
+    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: repo_site + 'img2/31.jpg'},
+    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: repo_site + 'img2/32.jpg'},
+    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: repo_site + 'img2/33.jpg'},
+    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: repo_site + 'img2/34.jpg'},
+    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: repo_site + 'img2/35.jpg'},
+    {adjective1: ["良いー悪い"], adjective2: ["好きなー嫌いな"], img: repo_site + 'img2/36.jpg'}
+];
+
+var test_stimuli_set4 = [
+    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: repo_site + 'img2/37.jpg'},
+    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: repo_site + 'img2/38.jpg'},
+    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: repo_site + 'img2/39.jpg'},
+    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: repo_site + 'img2/40.jpg'},
+    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: repo_site + 'img2/41.jpg'},
+    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: repo_site + 'img2/42.jpg'},
+    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: repo_site + 'img2/43.jpg'},
+    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: repo_site + 'img2/44.jpg'},
+    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: repo_site + 'img2/45.jpg'},
+    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: repo_site + 'img2/46.jpg'},
+    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: repo_site + 'img2/47.jpg'},
+    {adjective1: ["明るいー暗い"], adjective2: ["軽いー重い"], img: repo_site + 'img2/48.jpg'},
+];
+
   
 // すべての画像を1つのリストにまとめる
 var all_stimuli = test_stimuli_set1.concat(test_stimuli_set2, test_stimuli_set3, test_stimuli_set4);
@@ -100,7 +101,7 @@ var image = random_order[0].img;
 var currentStimulus = random_order[0]; // ランダムに選ばれた画像に関連する形容詞対を取得
 var condition_trial = {
     type: "html-keyboard-response",
-    stimulus: `<p>以下の項目について絵画を5段階で評価してもらいます。</p><br><p>${currentStimulus.adjective1}</p><p>${currentStimulus.adjective2}</p>`,
+    stimulus: `<p>以下の項目について絵画を5段階で評価してもらいます。</p><br><p><strong>${currentStimulus.adjective1}</strong></p><p><strong>${currentStimulus.adjective2}</strong></p><br>enterキーで次に進みます。`,
     choices: ["Enter"],  // Enterキーで次のステップに進む
 };
 timeline.push(condition_trial);
@@ -217,7 +218,7 @@ var space_key_trial = {
     
     on_finish: function(data){
        data.correct = reactionTime;
-       data.art = selectedImage;
+       data.art = image;
    }
 };
 
