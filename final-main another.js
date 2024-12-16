51cf24e2608af6101c088ad2977fda5caa96b0f5
sakurai Yuki <c6301309@st.kanazawa-it.ac.jp>
2024-12-16 13:16:02 +0900

    a

--- final-main another.js
+++ final-main another.js
@@ -237,7 +237,7 @@ var space_key_trial = {
     
     on_finish: function(data){
         data.correct = reactionTime; //jsPsych.timelineVariable("reactionTime");
-        data.art = selectedImage;  // 画像URLをデータとして保存
+        data.art = currentStimulus;  // 画像URLをデータとして保存
         console.log("data.art")
     }
 };
